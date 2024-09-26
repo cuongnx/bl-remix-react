999e023c56cf5532dd14b99772c156cff78d4aec
cuongnx <cuongnx.9x@gmail.com>
2024-09-26 14:43:06 +0700

    add eslint, prettier

--- server.ts
+++ server.ts
@@ -1,32 +1,31 @@
-import "dotenv/config";
-import { createRequestHandler } from "@remix-run/express";
-import express from "express";
+import 'dotenv/config';
+import { createRequestHandler } from '@remix-run/express';
+import express from 'express';
 
 const app = express();
 let build = null;
 
-if (process.env.NODE_ENV !== "production") {
-  const viteDevServer = await import("vite").then((vite) =>
+if (process.env.NODE_ENV !== 'production') {
+  const viteDevServer = await import('vite').then((vite) =>
     vite.createServer({
       server: { middlewareMode: true },
-    })
+    }),
   );
   app.use(viteDevServer.middlewares);
 
   const ssrModule = await viteDevServer.ssrLoadModule(
-    "virtual:remix/server-build"
+    'virtual:remix/server-build',
   );
   build = ssrModule.entry.module;
 } else {
-  app.use(express.static("build/client"));
+  app.use(express.static('build/client'));
   // @ts-ignore - the file might not exist yet but it will
-  // eslint-disable-next-line import/no-unresolved
-  build = await import("build/server/remix.js");
+  build = await import('build/server/remix.js');
 }
 
-app.all("*", createRequestHandler({ build }));
+app.all('*', createRequestHandler({ build }));
 
-const port = process.env.PORT ||  process.env.DEV_PORT;
+const port = process.env.PORT || process.env.DEV_PORT;
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
